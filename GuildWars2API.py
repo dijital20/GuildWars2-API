49b639a22752926a21859276e5d26ad050e8afc9
Josh Schneider <josh.schneider@gmail.com>
2018-01-22 20:33:41 -0600

    Moar logging, and did some repr() refinements.

--- GuildWars2API.py
+++ GuildWars2API.py
@@ -10,7 +10,7 @@ from urllib.request import Request, HTTPError, URLError, urlopen
 
 # Module level log.
 MODULE_LOG = logging.getLogger('GuildWars2API')
-MODULE_LOG.setLevel(logging.INFO)
+MODULE_LOG.setLevel(logging.DEBUG)
 # File Handler
 file_handler = logging.FileHandler(
     'GuildWars2API.log', mode='w', encoding='utf-8')
@@ -22,24 +22,39 @@ MODULE_LOG.addHandler(file_handler)
 
 
 class AuthorizationRequiredError(Exception):
+    """
+    Exception to be raised if an API requires authorization and no token is
+    available.
+    """
     pass
 
 
 class TokenMissingScope(Exception):
+    """
+    Exception to be raised if an API requires a scope that the current token
+    does not have.
+    """
     pass
 
 
 class APIError(Exception):
+    """
+    Error to be raised if we encounter communication errors.
+    """
     pass
 
 
 class GW2APISession(object):
+    """Session object. Keeps the token."""
     _base_url = 'https://api.guildwars2.com'
 
     def __init__(self):
+        """
+        Prepares a session for use.
+        """
         self.__token = None
         self.token_info = None
-        self._log.debug(f'Initialized {repr(self)}')
+        self._log.debug(f'Initialized {self}')
 
     def __repr__(self):
         state = 'Unauthenticated' \
@@ -49,10 +64,15 @@ class GW2APISession(object):
 
     @property
     def token(self):
+        """Get the token value."""
         return self.__token
 
     @token.setter
     def token(self, value):
+        """
+        Sets the token value, and makes an API call to get token info, storing
+        it in self.token_info.
+        """
         self._log.debug(f'Setting token to {value}')
         self.__token = value
         self.token_info = Token(session=self)
@@ -60,16 +80,41 @@ class GW2APISession(object):
 
     @property
     def _log(self):
+        """Logger"""
         return logging.getLogger(f'GuildWars2API.{self.__class__.__name__}')
 
     @property
     def _headers(self):
+        """Get a dictionary of headers."""
         if self.token:
             return {'Authorization': f'Bearer {self.token}'}
         return {}
 
     def make_request(self, url, params=None, data=None, headers=None,
                      auth=False):
+        """
+        Make an API request.
+
+        Args:
+            url (str): Endpoint URL to call.
+            params (dict, optional): Parameters to add to the URL.
+            data (dict, optional): Data to send in the request payload.
+            headers (dict, optional): Headers to send on the request. Starts
+                with the self._headers, and updates that dictionary with these
+                values.
+            auth (bool): Should we require authorization on this request?
+                Defaults to False. If true, and no token is present,
+                will raise an exception.
+
+        Returns (dict):
+            Dictionary converted from the JSON data returned.
+
+        Raises:
+            AuthorizationRequiredError: If the endpoint required
+                authorization, but none is provided.
+            APIError: If we have trouble contacting the endpoint, or the data
+                that comes back can't be converted properly.
+        """
         if auth and not self.token:
             self._log.error(f'auth specified, but token = {repr(self.token)}')
             raise AuthorizationRequiredError(
@@ -97,16 +142,30 @@ class GW2APISession(object):
             raise APIError from e
 
     def load_token(self, file_path):
+        """
+        Load a token from a file.
+
+        Args:
+            file_path (str): Path to the file containing the token.
+        """
         self._log.debug(f'Loading token from {file_path}')
         with open(file_path, mode='r') as f:
             self.token = f.read()
 
 
 class GW2API(object):
+    """Parent class for an API endpoint. Other classes derive off of this."""
     _endpoint_url = ''
     _required_scopes = []
 
     def __init__(self, session=None):
+        """
+        Prepare a GW2API object for use.
+
+        Args:
+            session (GW2APISession, optional): A session object. Defaults to
+                None, which results in a new session.
+        """
         self._log.debug(
             f'{self.__class__.__name__} init with session {session}'
         )
@@ -126,33 +185,59 @@ class GW2API(object):
 
     @property
     def _log(self):
+        """Logger"""
         return logging.getLogger(f'GuildWars2API.{self.__class__.__name__}')
 
 
 class GW2Thing(GW2API):
+    """
+    A class representing a single item (an Item, Recipe, Character,
+    etc). Properties are dynamically set based on the API return value.
+
+    Ensure the _endpoint_url class variable is set specific for the item.
+    """
     def __init__(self, id=None, session=None, auth=False):
+        """
+        Prepares a GW2Thing for use.
+
+        Args:
+            id (str, optional): An ID that the item corresponds to.
+            session (GW2APISession, optiona): Session to make requests from.
+                Defaults to None, which results in a new session. Session is
+                passed to child objects.
+            auth (bool, optional): Should the request required
+            authentication? Defaults to False.
+        """
         super(GW2Thing, self).__init__(session=session)
-        if id is None:
-            params = {}
-        else:
-            params = {'id': id}
-        info = self._session.make_request(self._endpoint_url, params=params,
-                                          auth=auth)
+        params = {} if id is None else {'id': id}
+        info = self._session.make_request(
+            self._endpoint_url, params=params, auth=auth)
         self._log.debug(f'Response:\n{pformat(info)}')
         self._update_obj(info)
+        self._log.info(f'Initialized {self}')
 
     def __repr__(self):
+        """repr() output"""
         item_name = self.__class__.__name__
         item_desc = getattr(self, "name", getattr(self, "id", None))
         return f'<{item_name} "{item_desc}">'
 
     def _update_obj(self, info):
+        """
+        Add the properties from a dictionary to the object, logging each one.
+
+        Args:
+            info (dict): Dictionary with the new values.
+        """
+        self._log.debug(f'Updating {self} with: {info}')
         for k, v in info.items():
             self._log.debug(f'Setting {self.__class__.__name__}.{k} to {v}')
             self.__dict__[k] = v
+        self._log.debug(f'Updated {self}')
 
     @property
     def details(self):
+        """Get a nice string representation of all properties on the object."""
         return f'{repr(self)}\n' + \
                '\n'.join([f'{k:>20}: {v}'
                           for k, v in self.__dict__.items()
@@ -160,21 +245,47 @@ class GW2Thing(GW2API):
 
 
 class GW2List(GW2API):
+    """
+    A class representing a list of things, either passed to the class,
+    or gleaned from an API. The class will be initialized with with the list
+    of ids or make a single request for the items.
+
+    Calling refresh() will cause the list to go make threaded API calls for
+    the individual items.
+
+    Ensure that the _endpoint_url and _thing_type are set for subclasses.
+    _thing_type should be a GW2Thing subclass.
+    """
     _thing_type = GW2Thing
 
     def __init__(self, session=None, auth=False, ids=None):
+        """
+        Prepares a GW2List for initial use.
+
+        Args:
+            session (GW2APISession, optional): Session to use to make requests.
+            auth (bool, optional): Should the call be authorized?
+            ids (list or tuple, optional): List or tuple of Ids. Defaults to
+                None (which makes an API call).
+        """
         super(GW2List, self).__init__(session=session)
         self._things = None
         self.count = 0
         self._auth = auth
-        self._ids = ids
+        self._ids = ids if ids else self._session.make_request(
+                self._endpoint_url, auth=self._auth)
+        self._log.info(f'Initialized {self}')
 
     def __iter__(self):
+        """
+        Iterate over the items. Causes a refresh if it hasn't been previously.
+        """
         if self._things is None:
             self.refresh()
         return iter(self._things)
 
     def __repr__(self):
+        """repr() output"""
         item_name = self._thing_type.__name__
         items = f'{len(self._things)} {item_name} items' \
             if self._things is not None \
@@ -184,18 +295,19 @@ class GW2List(GW2API):
         return f'<{self.__class__.__name__} {items}>'
 
     def refresh(self):
+        """
+        Go get all of the items and wrap them into their item classes. This
+        method uses a ThreadPool to make all of the API calls in parallel,
+        which uses a number of threads equal to the number of CPU cores.
+        """
         self._log.info(f'Refreshing {self}')
+        self._log.debug(f'ID list to query:\n{pformat(self._ids)}')
         timer = time.time()
-        if self._ids is None:
-            things_to_get = self._session.make_request(
-                self._endpoint_url, auth=self._auth)
-        else:
-            things_to_get = self._ids
-        with ThreadPool() as pool:
+        with ThreadPool(1) as pool:
             got_things = pool.map(
                 self.get_thing,
                 [(self._session, self._thing_type, t)
-                 for t in things_to_get]
+                 for t in self._ids]
             )
         self._things = [t for t in got_things if t is not None]
         self.count = len(self._things)
@@ -204,6 +316,18 @@ class GW2List(GW2API):
 
     @staticmethod
     def get_thing(args):
+        """
+        Static method called by the ThreadPool to make API calls and get
+        objects.
+
+        Args:
+            args (tuple): A 3 element tuple containing the session,
+                thing_type, and thing to get.
+
+        Returns (GWThing):
+            A GW2Thing object of thing_type type that represents the item.
+        """
+        MODULE_LOG.debug(f'get_thing with: {args}')
         session, thing_type, thing = args
         if isinstance(thing, dict) and thing.get('id'):
             thing_obj = thing_type(thing.get('id'), session=session)
@@ -216,15 +340,40 @@ class GW2List(GW2API):
 
 
 class GW2Enum(GW2API):
+    """
+    A class representing an Enumeration of types. Usually, these types allow
+    searching for specifics items based on criteria.
+
+    Ensure _thing_type is set in subclasses.
+    """
     _thing_type = GW2Thing
 
     def __init__(self, session=None):
+        """
+        Prepare a GW2Enum for use.
+
+        Args:
+            session (GW2APISession, optional): Session to use to make calls.
+                Defaults to None, which is replaced with a new session.
+        """
         super(GW2Enum, self).__init__(session=session)
+        self._log.info(f'Initialized {self}')
 
     def __repr__(self):
+        """repr() output."""
         return f'<{self.__class__.__name__} of {self._thing_type.__name__}>'
 
-    def get(self, id):
+    def get(self, id=None):
+        """
+        Get a specified item by id.
+
+        Args:
+            id (str, optional): ID to call for. Defaults to None, which is
+                replaced with 'all'.
+
+        Returns (GW2Thing):
+            A GW2Thing of the type in self._thing_type with the object.
+        """
         if id is None:
             id = 'all'
         if any(isinstance(id, t) for t in (list, tuple)):
@@ -241,17 +390,23 @@ class GW2Enum(GW2API):
 
 
 class Token(GW2Thing):
+    """Token object"""
     _endpoint_url = 'v2/tokeninfo'
 
     def __repr__(self):
-        return f'<{self.__class__.__name__} "{self.name}" with scopes ' \
-               f'{", ".join(self.permissions)}>'
+        item_name = getattr(self, 'name', 'Unknown')
+        item_perms = getattr(self, 'permissions', [])
+        return f'<{self.__class__.__name__} "{item_name}" with scopes ' \
+               f'{", ".join(item_perms)}>'
 
     def __str__(self):
-        return f'"{self.name}" with scopes {", ".join(self.permissions)}'
+        item_name = getattr(self, 'name', 'Unknown')
+        item_perms = getattr(self, 'permissions', [])
+        return f'"{item_name}" with scopes {", ".join(item_perms)}'
 
 
 class Account(GW2Thing):
+    """Account object"""
     _endpoint_url = 'v2/account'
     _required_scopes = ['account']
 
@@ -260,11 +415,12 @@ class Account(GW2Thing):
         self.world = World(self.world, session=session)
         self.guilds = MyGuilds(ids=self.guilds, session=session)
         self.bank = Bank(session=session)
-        self.characters = Characters(session=session)
+        self.characters = MyCharacters(session=session)
         self._log.debug(f'Account initialized with {self.__dict__}')
 
 
 class Character(GW2Thing):
+    """Character object"""
     _endpoint_url = 'v2/characters'
     _required_scopes = ['characters']
 
@@ -275,13 +431,15 @@ class Character(GW2Thing):
         self.equipment = MyEquipment(ids=self.equipment, session=session)
 
 
-class Characters(GW2List):
+class MyCharacters(GW2List):
+    """Collection of Characters for Account"""
     _endpoint_url = 'v2/characters'
     _thing_type = Character
     _required_scopes = ['characters']
 
 
 class Guild(GW2Thing):
+    """Guild object"""
     _endpoint_url = 'v2/guild'
 
     def __init__(self, id, session=None):
@@ -290,54 +448,70 @@ class Guild(GW2Thing):
 
 
 class Guilds(GW2Enum):
+    """Collection of Guilds"""
     _endpoint_url = 'v2/guild'
     _thing_type = Guild
 
 
 class MyGuilds(GW2List):
+    """Collection of Guilds for Account"""
     _thing_type = Guild
 
 
 class World(GW2Thing):
+    """World object"""
     _endpoint_url = 'v2/worlds'
 
 
 class Worlds(GW2Enum):
+    """Collection of Worlds"""
     _endpoint_url = 'v2/worlds'
     _thing_type = World
 
 
 class Item(GW2Thing):
+    """Item object"""
     _endpoint_url = 'v2/items'
 
 
 class Items(GW2Enum):
+    """Collection of items"""
     _endpoint_url = 'v2/items'
     _thing_type = Item
 
 
 class Recipe(GW2Thing):
+    """Recipe object"""
     _endpoint_url = 'v2/recipes'
 
 
 class Recipes(GW2Enum):
+    """Collection of Recipes"""
     _endpoint_url = 'v2/recipes'
     _thing_type = Recipe
 
 
 class MyRecipes(GW2List):
+    """Collection of Recipes on a Character"""
     _thing_type = Recipe
 
 
 class EquippedItem(Item):
-    pass
+    """An Equipped Item object"""
+    def __repr__(self):
+        item_name = self.__class__.__name__
+        item_desc = getattr(self, 'name', getattr(self, 'id', 'Unknown'))
+        item_slot = getattr(self, 'slot', 'Unknown')
+        return f'<{item_name} "{item_desc}" in slot {item_slot}>'
 
 
 class MyEquipment(GW2List):
+    """Collection of Equipped Item on a Character"""
     _thing_type = EquippedItem
 
 
 class BankItem(Item):
+    """An Item in the bank."""
     def __repr__(self):
         item_name = self.__class__.__name__
         item_desc = getattr(self, 'name', getattr(self, 'id', None))
@@ -346,6 +520,7 @@ class BankItem(Item):
 
 
 class Bank(GW2List):
+    """Collection of Bank Items on an Account"""
     _endpoint_url = 'v2/account/bank'
     _thing_type = BankItem
     _required_scopes = ['inventories']
@@ -375,4 +550,6 @@ if __name__ == '__main__':
     print('Characters:\n')
     for c in me.characters:
         print(c.details)
+        for e in c.equipment:
+            print(e)
         print()
